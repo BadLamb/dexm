f14ae463bf27137484551ef9e8c756d81dad735a
BadLamb <me@epsilon.pw>
2017-07-13 15:08:41 +0200

    Finish P2P and DB

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,66 +1,113 @@
 package blockchain
 
 import (
-	"time"
 	"bytes"
 	"encoding/binary"
+	"strconv"
+	"time"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/minio/blake2b-simd"
+	log "github.com/sirupsen/logrus"
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
-type Block struct{
-	Index int
-	Timestamp int64
+type Block struct {
+	Index             int
+	Timestamp         int64
+	Hash              string
 	PreviousBlockHash string
-	TransactionList []byte
-	ContractList []byte
+	TransactionList   []byte
+	ContractList      []byte
 }
 
-func (b *Block) CalculateHash() string{
+func (b *Block) CalculateHash() string {
 	// Convert struct to binary in order to hash
 	buf := new(bytes.Buffer)
 	err := binary.Write(buf, binary.LittleEndian, b)
 	if err != nil {
 		log.Error(err)
 	}
-	
+
 	hash := blake2b.Sum256(buf.Bytes())
 	return string(hash[:])
 }
 
-type BlockChain struct{
-	Blocks []Block
+type BlockChain struct {
+	DB *leveldb.DB
 }
 
 func NewBlockChain() *BlockChain {
+	bc := OpenBlockchain()
 	// generate Genesis Block
 	genesis := Block{
-		Index: 0,
-		Timestamp: time.Now().Unix(),
+		Timestamp:         time.Now().Unix(),
 		PreviousBlockHash: "",
-		TransactionList: []byte("Donald Trump Jr was wrong to meet Russian, says FBI chief Christopher Wray"),
+		TransactionList:   []byte("Donald Trump Jr was wrong to meet Russian, says FBI chief Christopher Wray"),
 	}
 
-	bc := BlockChain{
-		Blocks: make([]Block, 1),
+	hash := genesis.CalculateHash()
+	genesis.Hash = hash
+	bc.DB.Put([]byte(string("len")), []byte(string(1)), nil)
+	bc.DB.Put([]byte(string(0)), genesis.GetBytes(), nil)
+
+	return bc
+}
+
+func OpenBlockchain() *BlockChain {
+	db, err := leveldb.OpenFile("blockchain.db", nil)
+	if err != nil {
+		log.Fatal(err)
 	}
-	bc.Blocks[0] = genesis
-	return &bc
+
+	return &BlockChain{db}
 }
 
-func (bc *BlockChain) GetLatestBlock() *Block {
-	return &bc.Blocks[len(bc.Blocks) - 1]
+func (bc *BlockChain) GetLen() int {
+	index, err := bc.DB.Get([]byte(string("len")), nil)
+	if err != nil {
+		log.Fatal("Invalid blockchain.db")
+	}
+	num, _ := strconv.Atoi(string(index))
+	return num
 }
 
-func (bc *BlockChain) NewBlock(transactionList, contractList []byte){
+func (bc *BlockChain) GetBlock(index int) (*Block, error) {
+	data, err := bc.DB.Get([]byte(string(index)), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var newBlock Block
+	binary.Read(bytes.NewReader(data), binary.LittleEndian, &newBlock)
+
+	return &newBlock, nil
+}
+
+func (bc *BlockChain) NewBlock(transactionList, contractList []byte) {
 	var newB Block
-	latestBlock := bc.GetLatestBlock()
+	lastIndex := bc.GetLen() - 1
+	latestBlock, err := bc.GetBlock(lastIndex)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
 	newB.Index = latestBlock.Index + 1
 	newB.Timestamp = time.Now().Unix()
 	newB.PreviousBlockHash = latestBlock.CalculateHash()
 	newB.TransactionList = transactionList
 	newB.ContractList = contractList
-	
-	bc.Blocks = append(bc.Blocks, newB)
+
+	bc.DB.Put([]byte(string(lastIndex+1)), newB.GetBytes(), nil)
+	bc.DB.Put([]byte(string("len")), []byte(string(lastIndex+2)), nil)
+}
+
+func (b *Block) GetBytes() []byte {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.LittleEndian, b)
+	if err != nil {
+		log.Error(err)
+	}
+
+	return buf.Bytes()
 }
--- sync/protocol.go
+++ sync/protocol.go
@@ -1,69 +1,92 @@
 package protocol
 
-import(
-    "net/http"
-    "time"
-    "encoding/binary"
-    "encoding/json"
+import (
+	"encoding/binary"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"time"
 
-    "github.com/syndtr/goleveldb/leveldb"
-    "github.com/badlambd/dexm/blockchain"
-    log "github.com/sirupsen/logrus"
+	"github.com/badlamb/dexm/blockchain"
+	log "github.com/sirupsen/logrus"
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 const (
-    PORT = ":3141"
+	PORT = ":3141"
 )
 
 var nodeDatabase *leveldb.DB
+var bc *blockchain.BlockChain
 
 func StartSyncServer() {
-    log.Info("Opening node db..")
-    
-    chain := blockchain.NewBlockChain()
-    
-    nodeDatabase, _ = leveldb.OpenFile("ips.db", nil)
-    defer nodeDatabase.Close()
+	log.Info("Opening node db..")
 
-    log.Info("Starting sync webserver...")
-    http.HandleFunc("/getaddr", getAddr)
-    http.ListenAndServe(PORT, nil)
+	chain := blockchain.NewBlockChain()
+	bc = chain
+
+	nodeDatabase, _ = leveldb.OpenFile("ips.db", nil)
+	defer nodeDatabase.Close()
+
+	log.Info("Starting sync webserver...")
+	http.HandleFunc("/getaddr", getAddr)
+	http.HandleFunc("/getlen", getMaxBlock)
+	http.ListenAndServe(PORT, nil)
 }
 
 /* getAddr is an http request that returns all known ips */
-func getAddr(w http.ResponseWriter, r* http.Request) {
-    iter := nodeDatabase.NewIterator(nil, nil)
+func getAddr(w http.ResponseWriter, r *http.Request) {
+	iter := nodeDatabase.NewIterator(nil, nil)
 
-    ips := make(map[string][]byte)
+	ips := make(map[string][]byte)
 
-    for iter.Next() {
-        ips[string(iter.Key())] = iter.Value()
-    }
-    iter.Release()
+	for iter.Next() {
+		ips[string(iter.Key())] = iter.Value()
+	}
+	iter.Release()
 
-    updateTimestamp(r.RemoteAddr)
-    
-    value, err := json.Marshal(ips)
-    if err != nil{
-        log.Error(err)
-    }
+	updateTimestamp(r.RemoteAddr)
 
-    w.Write(value)
+	value, err := json.Marshal(ips)
+	if err != nil {
+		log.Error(err)
+	}
+
+	w.Write(value)
 }
 
 /* getBlock returns a block at index ?index */
-func getBlock(w http.ResponseWriter, r* http.Request) {
+func getBlock(w http.ResponseWriter, r *http.Request) {
+	if r.Form.Get("index") != "" {
+		index, err := strconv.Atoi(r.Form.Get("index"))
+		if err != nil {
+			w.Write([]byte("Error"))
+			return
+		}
+
+		data, err := bc.GetBlock(index)
+		if err != nil {
+			w.Write([]byte("Error"))
+			return
+		}
+
+		w.Write(data.GetBytes())
+	}
+}
+
+/* Returns how many blocks the client knows */
+func getMaxBlock(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(string(bc.GetLen())))
 }
 
 /* getMessage recives messages from other known peers about
    events(transactions, blocks etc)*/
-func getMessage(w http.ResponseWriter, r* http.Request) {}
-
+func getMessage(w http.ResponseWriter, r *http.Request) {}
 
 /* Insert IP and timestamp into DB */
 func updateTimestamp(ip string) {
-    log.Info(ip)
-    stamp := make([]byte, 8)
-    binary.LittleEndian.PutUint64(stamp, uint64(time.Now().Unix()))
-    nodeDatabase.Put([]byte(ip), stamp, nil)
+	log.Info(ip)
+	stamp := make([]byte, 8)
+	binary.LittleEndian.PutUint64(stamp, uint64(time.Now().Unix()))
+	nodeDatabase.Put([]byte(ip), stamp, nil)
 }

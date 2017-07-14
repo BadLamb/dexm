0fafcbb882748fbbc56084d6ee539f8553e87f1a
BadLamb <me@epsilon.pw>
2017-07-14 15:02:36 +0200

    Add message sharing

--- sync/protocol.go
+++ sync/protocol.go
@@ -2,9 +2,10 @@ package protocol
 
 import (
 	"encoding/json"
-	"net/http"
-	"strconv"
 	"net"
+	"net/http"
+	"bytes"
+	"strconv"
 	"time"
 
 	"github.com/badlamb/dexm/blockchain"
@@ -16,6 +17,11 @@ import (
 const (
 	PORT = ":3141"
 )
+
+type Message struct {
+	id   int
+	data []byte
+}
 
 var nodeDatabase *leveldb.DB
 var bc *blockchain.BlockChain
@@ -29,8 +35,8 @@ func StartSyncServer() {
 	nodeDatabase, _ = leveldb.OpenFile("ips.db", nil)
 	defer nodeDatabase.Close()
 
-	// This goroutine contacts known nodes and asks for their ip list
-	go func(){
+	/* This goroutine contacts known nodes and asks for their ip list */
+	go func() {
 		iter := nodeDatabase.NewIterator(nil, nil)
 		netTransport := &http.Transport{
 			Dial: (&net.Dialer{
@@ -39,65 +45,48 @@ func StartSyncServer() {
 			TLSHandshakeTimeout: 5 * time.Second,
 		}
 		netClient := &http.Client{
-			Timeout: time.Second * 10,
+			Timeout:   time.Second * 10,
 			Transport: netTransport,
 		}
 		for iter.Next() {
-			// Clean up given IP and avoid getting tricked into ddosing a server
+			/* Clean up given IP and avoid getting tricked into ddosing a server */
 			ip := string(iter.Key())
 
-			resp, err := netClient.Get("http://" + ip + PORT + "/getaddr")
-			if err != nil{
+			data, err := makeRequest("http://"+ip+PORT+"/getaddr", netClient)
+			if err != nil {
 				log.Error(err)
 				continue
 			}
-
 
 			ips := make(map[string][]byte)
-			data, err := ioutil.ReadAll(resp.Body)
-			if err != nil{
-				log.Error(err)
-				continue
-			}
 
 			log.Info(string(data))
 
 			json.Unmarshal(data, &ips)
 
-			for k, v := range ips{
+			for k, v := range ips {
 				e := net.ParseIP(k)
-				if e == nil{
+				if e == nil {
 					continue
 				}
 
 				_, err = nodeDatabase.Get([]byte(k), nil)
-				if err != nil{
+				if err != nil {
 					nodeDatabase.Put([]byte(k), v, nil)
-					
-					// Once a new IP has been found contact it and ask it for the len of it's chain
-					go func(k string){
-						resp, err := netClient.Get("http://" + k + PORT + "/getlen")
-						if err != nil{
-							log.Error(err)
-							return
-						}
 
-						data, err := ioutil.ReadAll(resp.Body)
-						if err != nil{
-							log.Error(err)
-							return
-						}
-
+					/* Once a new IP has been found contact it and ask it for the len of it's chain */
+					go func(k string) {
+						data, err := makeRequest("http://"+k+PORT+"/getlen", netClient)
 						numOfBlocks, err := strconv.Atoi(string(data))
-						if err != nil{
+						if err != nil {
 							log.Error(err)
 							return
 						}
-						if numOfBlocks > bc.GetLen(){
-							log.Info("Found peer with longer chain! Syncing...")
+						if numOfBlocks > bc.GetLen() {
+							log.Info("Found peer with longer chain! Need to sync this amount of blocks:", numOfBlocks-bc.GetLen())
 						}
 					}(k)
-					
+
 					continue
 				}
 
@@ -112,6 +101,7 @@ func StartSyncServer() {
 	http.HandleFunc("/getaddr", getAddr)
 	http.HandleFunc("/getlen", getMaxBlock)
 	http.HandleFunc("/getblock", getBlock)
+	http.HandleFunc("/newmsg", getMessage)
 	http.ListenAndServe(PORT, nil)
 }
 
@@ -163,7 +153,37 @@ func getMaxBlock(w http.ResponseWriter, r *http.Request) {
 
 /* getMessage recives messages from other known peers about
    events(transactions, blocks etc)*/
-func getMessage(w http.ResponseWriter, r *http.Request) {}
+func getMessage(w http.ResponseWriter, r *http.Request) {
+	log.Info(ioutil.ReadAll(r.Body))
+}
+
+func broadcastMessage(class int, data []byte) {
+	toSend := Message{class, data}
+
+	iter := nodeDatabase.NewIterator(nil, nil)
+	netTransport := &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
+	netClient := &http.Client{
+		Timeout:   time.Second * 10,
+		Transport: netTransport,
+	}
+
+	jsonStr, _ := json.Marshal(toSend)
+
+	for iter.Next() {
+		req, err := http.NewRequest("POST", "http://" + string(iter.Key()) + "/newmsg" , bytes.NewBuffer(jsonStr))
+		if err != nil{
+			continue
+		}
+
+		netClient.Do(req)
+	}
+
+}
 
 /* Insert IP and timestamp into DB */
 func updateTimestamp(ip string) {
@@ -174,4 +194,20 @@ func updateTimestamp(ip string) {
 	}
 	stamp := []byte(string(time.Now().Unix()))
 	nodeDatabase.Put([]byte(ip), stamp, nil)
+}
+
+func makeRequest(url string, client *http.Client) ([]byte, error) {
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return data, nil
 }

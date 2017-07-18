0826e782afa9f094689b118074a0d17241304127
BadLamb <me@epsilon.pw>
2017-07-18 16:35:48 +0200

    add valid ranges computation and hash verification for schelling

--- schelling/schelling.go
+++ schelling/schelling.go
@@ -1,8 +1,16 @@
 package schelling
 
+/*
+Schelling is a system that enables distributed datastreams on 
+the blockchain. It was developed by Vitalik Buterin and you
+can read about it here: https://blog.ethereum.org/2014/03/28/schellingcoin-a-minimal-trust-universal-data-feed/
+*/
+
 import(
     "encoding/json"
+    "sort"
 
+    "github.com/badlamb/dexm/sync"
     "github.com/minio/blake2b-simd"
 )
 
@@ -21,4 +29,17 @@ func ComputeShellingHash(wallet string, value int) []byte{
     sum := blake2b.Sum256(encoded)
     
     return sum[:]
+}
+
+func ComputeValidRange(knownSolutions []int) (min, max int){
+    sort.Ints(knownSolutions)
+
+    lowerBound := (len(knownSolutions)/100)*25
+    higherBound := (len(knownSolutions)/100)*75
+
+    return knownSolutions[lowerBound], knownSolutions[higherBound]
+}
+
+func VerifyHash(hash []byte, wallet string, solution int) bool{
+    return protocol.TestEq(hash, ComputeShellingHash(wallet, solution))
 }

05c4cf7b23741d53f15181705da3386de9f73118
BadLamb <me@epsilon.pw>
2017-07-31 22:33:40 +0200

    Add lyra2 pow

---
+++ blockchain/pow.go
@@ -0,0 +1,22 @@
+package blockchain
+
+import(
+    "math/big"
+
+    "github.com/bitgoin/lyra2rev2"
+)
+
+func SumDexmHashVOne(nonce, block []byte) (*big.Int, error){
+    toHash := append(nonce, block...)
+
+    result := new(big.Int)
+
+    hash, err := lyra2rev2.Sum(toHash)
+    if err != nil{
+        return result, err
+    }
+
+    result.SetBytes(hash)
+
+    return result, nil
+}
